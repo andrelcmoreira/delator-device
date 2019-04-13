32eb19e8165198797f5fb6aedcb4a9e22d5ebc13
André L. C. Moreira <alcm99@gmail.com>
2019-04-13 18:50:43 -0300

    Using boost_program_options lib to parse the cli arguments

--- src/app.cc
+++ src/app.cc
@@ -7,22 +7,10 @@
 
 boost::program_options::variables_map ParseCliArguments(int argc, char **argv)
 {    
-    boost::program_options::options_description desc("Supported options");
-    
-    desc.add_options()
-        ("server-ip", boost::program_options::value<std::string>(), 
-         "IP address of remote server")
-        ("gps-port", boost::program_options::value<std::string>(), 
-         "Port of GPS device")
-        ("gprs-port", boost::program_options::value<std::string>(), 
-         "Port of GPRS device");
-
     boost::program_options::variables_map vm;
-    auto args = boost::program_options::parse_command_line(argc, argv, desc);
     
-    boost::program_options::store(args, vm);
-    boost::program_options::notify(vm);
-
+    // TODO
+    
     return vm;
 }
 
@@ -33,12 +21,11 @@ int main(int argc, char **argv)
     try {
         auto args = ParseCliArguments(argc, argv);
         
-        delator::Daemon daemon{args["server-ip"].as<std::string>(), 
-                               args["gprs-port"].as<std::string>(), 
-                               args["gps-port"].as<std::string>()};
+        // TODO: pass the correct values to daemon constructor.
+        delator::Daemon daemon{"", "", ""};
         ret = daemon.Run();
     } catch (const std::exception &e) {
-        // TODO: handle errors.
+        // TODO: handle errors properly.
         std::cerr << e.what() << std::endl;
     }
          
