e5dd715e96232d6a1d26799a410ebebf13109a77
André L. C. Moreira <alcm99@gmail.com>
2019-04-28 19:48:39 -0300

    Audio classification proof of concept

---
+++ svm.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/ml.hpp>
+
+int main()
+{
+    auto data_set = cv::ml::TrainData::loadFromCSV("train_data.csv", 1, 0, 1);
+
+    data_set->setTrainTestSplitRatio(0.8); // 80 % of dataset for train purpose
+    
+    cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
+    svm->setType(cv::ml::SVM::C_SVC);
+    svm->setKernel(cv::ml::SVM::LINEAR);
+    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 
+                                          1e-6));
+    
+    // Setup train data
+    cv::Mat train_samples = data_set->getTrainSamples();
+    cv::Mat train_responses = data_set->getTrainResponses();
+    
+    train_responses.convertTo(train_responses, CV_32S);
+    svm->train(train_samples, cv::ml::ROW_SAMPLE, train_responses);
+    
+    // Setup test data 
+    cv::Mat test_samples = data_set->getTestSamples();
+    cv::Mat test_responses = data_set->getTestResponses();
+    cv::Mat results;
+    
+    svm->predict(test_samples, results);
+
+    float correct = countNonZero(results == test_responses);
+    float accuracy = correct / results.total();
+    
+    std::cout << "correct answers: " << correct << std::endl;
+    std::cout << "accuracy: " << accuracy << std::endl;
+
+    return 0;
+}
